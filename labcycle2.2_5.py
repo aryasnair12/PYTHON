91881f5f717eaa55204b837c2402d06719e037a0
arya s nair <20mp1544@rit.ac.in>
2021-02-14 02:49:27 +0530

    adding cycle 2

---
+++ labcycle2.2_5.py
@@ -0,0 +1,7 @@
+square=lambda x:x*x
+rectangle=lambda x,y:x*y
+triangle=lambda x,y:0.5*x*y
+
+print("area of square:",square(4))
+print("area of rectangle:",rectangle(8,9))
+print("area of triangle:",triangle(8,9))
