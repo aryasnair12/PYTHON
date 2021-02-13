830b10181617a2010a8e66a3366efb2420f86b25
arya s nair <20mp1544@rit.ac.in>
2021-02-13 22:13:59 +0530

    adding cycle 1.3

---
+++ labcycle1.3_3.py
@@ -0,0 +1,10 @@
+a=float(input("enter first number"))
+b=float(input("enter second numner"))
+c=float(input("enter third number"))
+if(a>b)and(a>c):
+    print("a is greater")
+elif(b>a)and(b>c):
+    print("b is greater")
+else:
+    print("c is greater")
+
