0264f08e67665cc294420f4691f4bcceb9d41b15
arya s nair <20mp1544@rit.ac.in>
2021-02-13 22:15:25 +0530

    adding cycle 1.3

---
+++ labcycle1.3_7.py
@@ -0,0 +1,5 @@
+list1=['red','orange','yellow','black']
+list2=['white','black','green']
+print("list1 elements are:",list1)
+print("list2 elements are:",list2)
+print(set(list1).difference(set(list2)))
